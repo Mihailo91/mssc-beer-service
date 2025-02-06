887783e67980b05efe4dd0926926564302c7da20
kmihailo <mihailo.krstic@eng.it>
2025-02-06 20:50:15 +0100

    Addd BeerMapper with mupstruct and lombok

--- src/main/java/guru/springframework/msscbeerservice/web/contoller/BeerController.java
+++ src/main/java/guru/springframework/msscbeerservice/web/contoller/BeerController.java
@@ -1,33 +1,35 @@
 package guru.springframework.msscbeerservice.web.contoller;
 
 import guru.springframework.msscbeerservice.web.model.BeerDto;
-import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.UUID;
 
+@Slf4j
 @RequestMapping("/api/v1/beer")
 @RestController
 public class BeerController {
 
     @GetMapping("/{beerId}")
     public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId) {
-        //TODO implement
+        log.info("getBeerById");
         BeerDto beerDto = new BeerDto();
         return new ResponseEntity<>(beerDto,HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity saveNewBeer(@Valid @RequestBody BeerDto beerDto) {
-        //TODO implement
+        log.info("saveNewBeer");
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
     public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @Valid @RequestBody BeerDto beerDto) {
-        //TODO implement
+        log.info("updateBeerById");
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
