35028ac1fa026f89dce80bacce82f574d8978c33
kmihailo <mihailo.krstic@eng.it>
2025-02-06 14:37:40 +0100

    Addd validation and error handling

--- src/main/java/guru/springframework/msscbeerservice/web/contoller/BeerController.java
+++ src/main/java/guru/springframework/msscbeerservice/web/contoller/BeerController.java
@@ -1,6 +1,7 @@
 package guru.springframework.msscbeerservice.web.contoller;
 
 import guru.springframework.msscbeerservice.web.model.BeerDto;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,13 +20,13 @@ public class BeerController {
     }
 
     @PostMapping
-    public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto) {
+    public ResponseEntity saveNewBeer(@Valid @RequestBody BeerDto beerDto) {
         //TODO implement
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
-    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto) {
+    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @Valid @RequestBody BeerDto beerDto) {
         //TODO implement
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
